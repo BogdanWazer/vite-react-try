778008d5f0ae25c07930daefbbc2d300b2e3eaee
BogdanWazer <ki4ack.bogdan@gmail.com>
2023-06-11 20:22:44 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,40 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-const favoriteMovie = 'StarWars';
-function App() {
+function User({ name, channelName, ...user }) {
   return (
     <div>
-      <h1>Hello, {favoriteMovie}</h1>
+      <h1>{name}</h1>
+      <p>{JSON.stringify(user)}</p>
+    </div>
+  );
+}
+
+function App() {
+  const socialNetwork = 'YouTube';
+  const fruits = ['banana', 'orange', 'apple'];
+  fruits.push('mango');
+  const [banana, orange, ...restFruits] = fruits;
+
+  // const newFruits = fruits.map(function (item, index) {
+  //   return `Fruit (${index}): ${item}`;
+  // });
+  // console.log(newFruits);
+
+  let newName = 'Denis';
+  const user = {
+    name: 'Maks',
+    age: 27,
+    youtubeChannel: 'MyYouTube',
+    [socialNetwork]: 'TravelsCode',
+    myFruits: ['banana', 'orange'],
+  };
+
+  const { name, ...restNames } = user;
+
+  return (
+    <div className="App">
+      <User {...user} />
     </div>
   );
 }
